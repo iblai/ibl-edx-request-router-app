2f94b1b03b1518cc21a15fb9215b7c3a4073be2e
Derek Chen <derek@ibleducation.com>
2020-01-02 03:33:22 -0500

    Add initial Django files

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import os
+from setuptools import find_packages, setup
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='dl-request-router',
+    version='0.0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    description='Request router',
+    url='https://gitlab.com/deeplms/dl-request-router',
+    author='IBL Studios',
+    author_email='ibl@ibl.ibl',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP'
+    ],
+)
