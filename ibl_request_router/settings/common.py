a479dfacabf84bb129038e7516fc3d9530d0db42
wizGipsBrian <gipsbrian@gmail.com>
2021-07-08 18:24:08 +0300

    moved settings to common

--- ibl_request_router/settings/common.py
+++ ibl_request_router/settings/common.py
@@ -22,5 +22,12 @@ def plugin_settings(settings):  # pylint: disable=unused-argument
     """
     Defines ibl_request_router-specific settings when app is used as a plugin to edx-platform.
     """
-    settings.IBL_REQUEST_ROUTER_BY_VIEWING_DELAY_MS = 5000
-    settings.IBL_REQUEST_ROUTER_VIDEO_COMPLETE_PERCENTAGE = 0.95
+    settings.MANAGER_BASE_URL = "https://studio.iblopen.ibl.dev"
+    settings.MANAGER_AUTH_ENABLED = True
+    settings.MANAGER_AUTH_APP_ID = "manager"
+    settings.MANAGER_DEFAULT_ORG = "main"
+    settings.MANAGER_MAX_TRIES = 5
+    settings.MANAGER_MULTITENANCY_ENABLED = False
+    settings.MANAGER_PROXY_TIMEOUT = 10
+    settings.MANAGER_REQUEST_TIMEOUT = 10
+    settings.MANAGER_VERIFY_SSL = True
