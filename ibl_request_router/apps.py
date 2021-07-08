17ccef3bbd0dd681157b96b99057a78cb44eaabf
wizGipsBrian <gipsbrian@gmail.com>
2021-07-08 10:22:26 +0300

    setup.py update setting/common.py for cms and lms

--- ibl_request_router/apps.py
+++ ibl_request_router/apps.py
@@ -1,5 +1,31 @@
+"""
+App Configuration for ibl_request_router
+"""
+
+
 from django.apps import AppConfig
 
-class APIAuthConfig(AppConfig):
+
+class RequestRouterConfig(AppConfig):
+    """
+    App Configuration for ibl_request_router
+    """
     name = 'ibl_request_router'
-    verbose_name = "IBL API Auth Management"
+    verbose_name = "IBL Request Router"
+
+    plugin_app = {
+        'settings_config': {
+            'lms.djangoapp': {
+                'common': {
+                    'relative_path': 'settings.common',
+                },
+            },
+        },
+        'settings_config': {
+            'cms.djangoapp': {
+                'common': {
+                    'relative_path': 'settings.common',
+                },
+            },
+        }
+    }
