c5fc77ac43a00defa23fc804574d4ed77fb47a73
Shinji <shinji@shinji.rocks>
2022-08-09 01:27:02 +0800

    add test

---
+++ tests/test_api_manager.py
@@ -0,0 +1,18 @@
+from unittest import mock
+
+import pytest
+from django.http import Http404
+
+from ibl_request_router.api.manager import manager_api_request
+
+MANAGER_BASE_URL_PKG_PATH = ("ibl_request_router.api.manager.MANAGER_BASE_URL",)
+
+
+@pytest.Mark.django_db
+class TestAPIManager:
+    endpoint = "knock_knock"
+
+    @mock.patch(MANAGER_BASE_URL_PKG_PATH, None)
+    def test_on_empty_base_url_returns_404(self):
+        with pytest.raises(Http404):
+            manager_api_request("get", self.endpoint)
