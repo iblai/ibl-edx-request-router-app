a492c9b9ab44317b55765062c17f1744dd18474a
Shinji <shinji@shinji.rocks>
2022-08-08 23:16:55 +0800

    add tests

--- tests/test_utils_org.py
+++ tests/test_utils_org.py
@@ -2,8 +2,16 @@ from unittest import mock
 
 import pytest
 from django.test import RequestFactory
+from xmodule.modulestore.tests.factories import ToyCourseFactory
 
-from ibl_request_router.utils.org import get_org, get_org_from_request
+from ibl_request_router.utils.org import (
+    get_org,
+    get_org_from_course,
+    get_org_from_course_key_string,
+    get_org_from_request,
+)
+
+from .utils import course_key
 
 DEFAULT_ORG_PKG_PATH = "ibl_request_router.utils.org.DEFAULT_ORG"
 DEFAULT_ORG = "UCL Psychopath Academy"
@@ -48,10 +56,31 @@ class TestUtilsOrg:
     @mock.patch(DEFAULT_ORG_PKG_PATH, DEFAULT_ORG)
     def test_get_org_from_request(self, multitenancy_enabled):
         request = RequestFactory()
-        request.get_host = lambda: '127.0.0.1'
+        request.get_host = lambda: "127.0.0.1"
         with mock.patch(MULTITENANCY_ENABLED_PKG_PATH, multitenancy_enabled):
             org = get_org_from_request(request)
         if multitenancy_enabled:
             assert org is None  # no microsite
         else:
             assert org == DEFAULT_ORG
+
+    @pytest.mark.parametrize(
+        "multitenancy_enabled",
+        (
+            True,
+            False,
+        ),
+    )
+    @mock.patch(DEFAULT_ORG_PKG_PATH, DEFAULT_ORG)
+    def test_get_org_from_course_or_key_or_string(self, multitenancy_enabled):
+        toy_course = ToyCourseFactory()
+        course_key_string = str(course_key(toy_course))
+        with mock.patch(MULTITENANCY_ENABLED_PKG_PATH, multitenancy_enabled):
+            org_from_course = get_org_from_course(toy_course)
+            org_from_string = get_org_from_course_key_string(course_key_string)
+        if multitenancy_enabled:
+            assert org_from_string == toy_course.org
+            assert org_from_course == toy_course.org
+        else:
+            assert org_from_string == DEFAULT_ORG
+            assert org_from_course == DEFAULT_ORG
