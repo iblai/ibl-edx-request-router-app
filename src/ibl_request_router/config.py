27ab449d492d983c8df6a346ddca9a89e7565641
Geoff Von Allmen <geoff@ibleducation.com>
2024-04-29 12:36:45 -0700

    feat: Add Api Token endpoint
    - Mv changes from original token proxy to new endpoint
    - This is to maintain backwards compatibility with origin impl.

--- src/ibl_request_router/config.py
+++ src/ibl_request_router/config.py
@@ -1,34 +1,38 @@
 from django.conf import settings
 
-
 MANAGER_BASE_URL = getattr(settings, "MANAGER_BASE_URL", "")
 MANAGER_BASE_API_URL = MANAGER_BASE_URL + "/api"
 
 # Request retries
-MANAGER_MAX_TRIES = getattr(settings, 'MANAGER_MAX_TRIES', 1)
+MANAGER_MAX_TRIES = getattr(settings, "MANAGER_MAX_TRIES", 1)
 # Request certificate verification
-MANAGER_VERIFY_SSL = getattr(settings, 'MANAGER_VERIFY_SSL', True)
+MANAGER_VERIFY_SSL = getattr(settings, "MANAGER_VERIFY_SSL", True)
 # Request default timeout
-MANAGER_REQUEST_TIMEOUT = getattr(settings, 'MANAGER_REQUEST_TIMEOUT', 20)
+MANAGER_REQUEST_TIMEOUT = getattr(settings, "MANAGER_REQUEST_TIMEOUT", 20)
 # Request proxy timeout
-MANAGER_PROXY_TIMEOUT = getattr(settings, "MANAGER_PROXY_TIMEOUT", MANAGER_REQUEST_TIMEOUT)
+MANAGER_PROXY_TIMEOUT = getattr(
+    settings, "MANAGER_PROXY_TIMEOUT", MANAGER_REQUEST_TIMEOUT
+)
 
 # Allow proxy requests to the manager through edX
 MANAGER_PROXY_ENABLED = getattr(settings, "MANAGER_PROXY_ENABLED", True)
 MANAGER_TOKEN_PROXY_ENABLED = getattr(settings, "MANAGER_TOKEN_PROXY_ENABLED", True)
 MANAGER_TOKEN_ENDPOINT_PATH = "core/token/proxy/"
+MANAGER_API_TOKEN_ENDPOINT_PATH = "core/api-token/proxy/"
 
-MANAGER_AUTH_ENABLED = getattr(settings, 'MANAGER_AUTH_ENABLED', True)
-MANAGER_AUTH_APP_ID = getattr(settings, 'MANAGER_AUTH_APP_ID', 'manager')
-
-
-DEFAULT_ORG = getattr(settings, 'MANAGER_DEFAULT_ORG', None)
-MULTITENANCY_ENABLED = getattr(settings, 'MANAGER_MULTITENANCY_ENABLED', False)
+MANAGER_AUTH_ENABLED = getattr(settings, "MANAGER_AUTH_ENABLED", True)
+MANAGER_AUTH_APP_ID = getattr(settings, "MANAGER_AUTH_APP_ID", "manager")
 
 
-MANAGER_API_UNAUTH_ALLOWLIST = getattr(settings, 'IBL_REQUEST_ROUTER_MANAGER_API_UNAUTH_ALLOWLIST', [])
-MANAGER_API_AUTH_ALLOWLIST = getattr(settings, 'IBL_REQUEST_ROUTER_MANAGER_API_AUTH_ALLOWLIST', [])
+DEFAULT_ORG = getattr(settings, "MANAGER_DEFAULT_ORG", None)
+MULTITENANCY_ENABLED = getattr(settings, "MANAGER_MULTITENANCY_ENABLED", False)
 
-URL_PATTERNS_BEFORE_ROUTER = getattr(
-    settings, 'IBL_URL_PATTERNS_BEFORE_ROUTER', []
+
+MANAGER_API_UNAUTH_ALLOWLIST = getattr(
+    settings, "IBL_REQUEST_ROUTER_MANAGER_API_UNAUTH_ALLOWLIST", []
 )
+MANAGER_API_AUTH_ALLOWLIST = getattr(
+    settings, "IBL_REQUEST_ROUTER_MANAGER_API_AUTH_ALLOWLIST", []
+)
+
+URL_PATTERNS_BEFORE_ROUTER = getattr(settings, "IBL_URL_PATTERNS_BEFORE_ROUTER", [])
--- src/ibl_request_router/views.py
+++ src/ibl_request_router/views.py
@@ -16,7 +16,10 @@ from edx_rest_framework_extensions.auth.jwt.authentication import JwtAuthenticat
 from rest_framework.response import Response
 
 from ibl_request_router.api.manager import manager_api_request, manager_proxy_request
-from ibl_request_router.config import MANAGER_TOKEN_ENDPOINT_PATH
+from ibl_request_router.config import (
+    MANAGER_API_TOKEN_ENDPOINT_PATH,
+    MANAGER_TOKEN_ENDPOINT_PATH,
+)
 from ibl_request_router.utils.access import check_request_permissions
 
 log = logging.getLogger(__name__)
@@ -87,16 +90,9 @@ def manager_token_proxy_view(request):
         log.warning("Invalid user cannot request token.")
         raise Http404
 
-    platform_key = request.data.get("platform_key")
-    if not platform_key:
-        log.warning("Missing platform_key - cannot request token without platform_key")
-        return Response({"error": "Missing platform_key"}, status=400)
-
     try:
         response = manager_api_request(
-            "POST",
-            MANAGER_TOKEN_ENDPOINT_PATH,
-            data={"user_id": request.user.id, "platform_key": platform_key},
+            "POST", MANAGER_TOKEN_ENDPOINT_PATH, data={"user_id": request.user.id}
         )
 
         try:
@@ -114,4 +110,55 @@ def manager_token_proxy_view(request):
         return Response({}, status=response.status_code)
     except Exception:
         log.exception("Token proxy request error")
+        raise Http404
+
+
+# Token view
+
+
+@api_view(["POST"])
+@csrf_exempt
+@authentication_classes(
+    (SessionAuthentication, OAuth2Authentication, JwtAuthentication)
+)
+def manager_api_token_proxy_view(request):
+    """
+    Same as
+
+    User must be authenticated.
+    """
+    # Check request user
+    if not (request.user and request.user.is_authenticated and request.user.is_active):
+        log.warning("Invalid user cannot request token.")
+        raise Http404
+
+    platform_key = request.data.get("platform_key")
+    if not platform_key:
+        log.warning(
+            "Missing platform_key - cannot request api-token without platform_key"
+        )
+        return Response({"error": "Missing platform_key"}, status=400)
+
+    try:
+        response = manager_api_request(
+            "POST",
+            MANAGER_API_TOKEN_ENDPOINT_PATH,
+            data={"user_id": request.user.id, "platform_key": platform_key},
+        )
+
+        try:
+            log.info("Api Token proxy response %s", response.status_code)
+            return Response(response.json(), status=response.status_code)
+        except ValueError:
+            if response.ok:
+                # Only log when the response is expected to be valid
+                log.exception(
+                    "Non-JSON api-token proxy response: %s %s",
+                    response.status_code,
+                    response.text,
+                )
+
+        return Response({}, status=response.status_code)
+    except Exception:
+        log.exception("Api Token proxy request error")
         raise Http404
